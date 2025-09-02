25bdb43066a45ba438807c04a4bab6762d9fe2ef
Jose Barragan Fernandez <javitricking@hotmail.com>
2025-09-02 02:19:18 +0200

    images next.config

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  images: {
+    unoptimized: true,
+  },
   async headers() {
     return [
       {
         source: '/(.*)',
         headers: [
+          
           {
             key: 'Content-Security-Policy',
             value: [
